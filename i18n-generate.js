1836a60ef1b7376cc775e0c4c857fca45d5404b0
Jesu Castillo <castillojesuj@gmail.com>
2017-09-07 14:56:37 -0300

    Added deepmerge for object generation, and object generation

--- i18n-generate.js
+++ i18n-generate.js
@@ -33,12 +33,92 @@ function sortObject(obj) {
   ), {});
 }
 
+function getObjectNestedProperties(obj,parent){
+  let props = [];
+  Object.keys(obj).forEach(key => {
+    if (typeof obj[key] === "object") {
+      let innerKeys = getObjectNestedProperties(obj[key], key);
+      innerKeys.forEach((innerKey,index,arr)=>{
+        arr[index] = `${key}.${innerKey}`;
+      });
+      props = props.concat(innerKeys);
+    } else {
+      props.push(key);
+    }
+  });
+  return props;
+}
+
+function buildObject(obj,key){
+  let value;
+  if (key.includes(".")) {
+    let keys = key.split(".");
+    obj[keys[0]] = buildObject(obj[keys[0]] ? obj[keys[0]] : {},keys.splice(1).join("."));
+  }else{
+    obj[key] = `${prefix}${key}`;
+  }
+  return obj;
+}
+
+function getObjectFromTranslations(tObject){
+  let obj = {};
+  let keys = getObjectNestedProperties(tObject);
+  keys.forEach((k)=>{
+    Object.assign(obj,buildObject(obj,k));
+  });
+  return obj;
+}
+
+function findInnerValue(obj,key){
+  let value;
+  if(key.includes(".")){
+    let keys = key.split(".");
+    if (obj[keys[0]]) {
+      return findInnerValue(obj[keys[0]], keys.splice(1).join("."));
+    }
+    return undefined;
+  }
+  return obj[key];
+}
+
+/**
+ * Simple object check.
+ * @param item
+ * @returns {boolean}
+ */
+function isObject(item) {
+  return (item && typeof item === 'object' && !Array.isArray(item));
+}
+
+/**
+ * Deep merge two objects.
+ * @param target
+ * @param ...sources
+ */
+function mergeDeep(target, ...sources) {
+  if (!sources.length) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return mergeDeep(target, ...sources);
+}
+
 const argv = require('minimist')(process.argv.slice(2));
 const dir = argv.s || argv.source;
 const functionName = argv.f || argv.functionName || '__';
 const outputDirectory = argv.o || argv.output || 'translations';
 const languages = argv.l || argv.languages || 'en';
-const prefix = argv.p || argv.prefix || '!!';
+const prefix = argv.p || argv.prefix || '!<';
 const willTransformise = argv.t || argv.transformise || false;
 
 if (!dir) console.error('no directory supplied. use -d');
@@ -51,29 +131,35 @@ glob(`${dir}/**/*.json`, {}, function(er, files) {
     _.uniq,
     _.flatten,
     _.map(function(file) {
-      return JSON.parse(fs.readFileSync(file, 'utf8'));
+      const text = fs.readFileSync(file, "utf8");
+      const fileObject = JSON.parse(text);
+      const result = getObjectNestedProperties(fileObject);
+      return result;
     })
   )(files);
-  const languagesArray = languages.split(' ');
+  const languagesArray = languages.split(" ");
   languagesArray.forEach(function(language) {
     const localeText = getLocaleConfig(outputDirectory, language);
-    const foundMap = _.keyBy(function(obj) {
-      return Object.keys(obj).sort();
+    const foundMap = _.keyBy(function(str) {
+      return willTransformise ? transformise(str) : str;
     })(value);
-    // finds new translations to add
     const newTranslations = _.pickBy(function(v, key) {
-      const found = localeText[key];
-      return !found || found.search(prefix) !== -1;
+      const found = key.includes(".")
+        ? findInnerValue(localeText, key)
+        : localeText[key];
+      return !found || found.startsWith(prefix);
     })(foundMap);
     console.log(`\n\n${language}: new translations found\n`, newTranslations);
-    let newObject = Object.assign({},
+    let newObject = mergeDeep(
+      {},
       localeText,
-      _.mapValues(function(str) { return `${prefix}${str}` })(newTranslations)
+      getObjectFromTranslations(newTranslations)
     );
     newObject = sortObject(newObject);
     fs.writeFileSync(
       `${outputDirectory}/${language}.json`,
-      JSON.stringify(newObject, null, 2), 'utf8'
+      JSON.stringify(newObject, null, 2),
+      "utf8"
     );
   });
 });
